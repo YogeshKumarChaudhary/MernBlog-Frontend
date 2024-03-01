d6788e91beb978221b1254bc82891bda2e9164ef
Chaudhary YogeshKumar <yogeshchaudhary9081@gmail.com>
2024-03-01 23:31:30 +0530

    data change

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -3,6 +3,7 @@ import "./Blog.css";
 import { Link } from "react-router-dom";
 
 const Blog = ({ data }) => {
+  console.log(data);
   return (
     <section className="blog-container">
       <div className="image">
